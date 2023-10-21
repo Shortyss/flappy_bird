ec5e351483c1811137a3f5d42ae0d845455ea5eb
Shortyss <Shortyss@seznam.cz>
2023-10-21 19:17:08 +0200

    UFO flying

--- ufo.py
+++ ufo.py
@@ -12,8 +12,16 @@ class Ufo():
         self.rect.midleft = self.screen_rect.midleft
         self.move_up = False
 
-        gravity = 1
-        velocity = 0
+
+        self.ufo_velocity = 0
+        self.gravity = 1
+
+    def update(self):
+        self.ufo_velocity += self.gravity
+        if self.move_up:
+            self.ufo_velocity = -10
+
+        self.rect.y += self.ufo_velocity
 
     def blit_me(self):
         self.screen.blit(self.image, self.rect)
