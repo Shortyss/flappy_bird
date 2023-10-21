3b954cf716742dd71641173e15ccf4fb9a521e44
Shortyss <Shortyss@seznam.cz>
2023-10-21 17:00:14 +0200

    working backgroud

--- main.py
+++ main.py
@@ -7,17 +7,36 @@ class Game:
     def __init__(self):
         pygame.init()
         self.settings = Settings()
-        # self.screen = pygame.display.set_mode(0, 0)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
-        self.settings.screen_width = self.settings.screen_width
-        self.settings.screen_height = self.settings.screen_height
+        pygame.display.set_caption("Space Adventure 2023")
+        self.clock = pygame.time.Clock()
+
+        self.backgroud = pygame.image.load("2205_w026_n002_1983b_p1_1983.jpg")
+        self.backgroud = pygame.transform.scale(self.backgroud, (self.settings.screen_width, self.settings.screen_height))
+        self.backgroud_x = 0
 
 
     def run(self):
         while True:
-            pass
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.backgroud_x -= 2
+
+            if self.backgroud_x <= -self.settings.screen_width:
+                self.backgroud_x = 0
+
+            self.screen.blit(self.backgroud, (self.backgroud_x, 0))
+            self.screen.blit(self.backgroud, (self.backgroud_x + self.settings.screen_width, 0))
+
+            pygame.display.flip()
+            self.clock.tick(self.settings.fps)
 
 
 if __name__ == "__main__":
     game = Game()
     game.run()
+
--- settings.py
+++ settings.py
@@ -1,8 +1,9 @@
-import pygame.image
+import pygame
 
 
 class Settings:
     def __init__(self):
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_screen = pygame.image.load("2204_w023_n001_2177b_p1_2177.jpg")
+        self.bg_screen = pygame.image.load("2205_w026_n002_1983b_p1_1983.jpg")
+        self.fps = 60
